0bbbc782c77b1cce6ddc9280bbf432f2bfe0ec7a
Chris Cantrell <chris.cantrell@vertivco.com>
2018-04-09 12:56:02 -0500

    Lots of work on python drivers

--- pi/hardware.py
+++ pi/hardware.py
@@ -1,21 +1,67 @@
 import RPi.GPIO as GPIO
+import smbus
 
 class LED8x8:
     
-    def __init__(self,bus,address):
-        self._bus = bus
+# LSB at top, MSB at bottom
+#                     col 7 green
+#                     |   col 7 red
+#                     |   |   col 6 green
+#                     |   |   |   col 6 red
+#                     |   |   |   |   col 5 green
+#                     |   |   |   |   |   col 5 red
+#                     |   |   |   |   |   |   col 4 green
+#                     |   |   |   |   |   |   |   col 4 red
+#                     |   |   |   |   |   |   |   |   col 3 green
+#                     |   |   |   |   |   |   |   |   |   col 3 red
+#                     |   |   |   |   |   |   |   |   |   |   col 2 green
+#                     |   |   |   |   |   |   |   |   |   |   |   col 2 red
+#                     |   |   |   |   |   |   |   |   |   |   |   |   col 1 green 
+#                     |   |   |   |   |   |   |   |   |   |   |   |   |   col 1 red
+#                     |   |   |   |   |   |   |   |   |   |   |   |   |   |   col 0 green
+#                     |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   col 0 red
+#                     |   |   |   |   |   |   |   |   |   |   |   |   |   |   |   |
+#disp.draw_raw_image([0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0])
+    
+    def __init__(self,address):
+        self._bus = smbus.SMBus(1)
         self._address = address;
         self._pixels = [0]*16        
-        self._bus.write_byte(self._address, 0x21)
-        self._bus.write_byte(self._address, 0xEF)
-        self._bus.write_byte(self._address, 0x81)        
+        self._bus.write_block_data(self._address, 0x21, [])
+        self._bus.write_block_data(self._address, 0xEF, [])
+        self._bus.write_block_data(self._address, 0x81, [])        
         self.refresh_display()
         
     def refresh_display(self):
-        self.draw_image(self._pixels)        
+        self.draw_raw_image(self._pixels)        
         
-    def draw_image(self, image):
-        self._bus.write_block_data(self._address, 0, image)
+    def draw_raw_image(self, image):
+        # TODO ... why do I have to start at 15?
+        self._bus.write_block_data(self._address, 15, image)
+        
+    def set_pixel(self,x,y,color):
+        if x<0 or x>7 or y<0 or y>7:
+            return
+        # 0=black, 1=green, 2=red, 3=orange
+        x = (7 - x)*2 # Point to green column
+        y = 1 << y
+        m = (~y)&255
+        self._pixels[x] = self._pixels[x] & m
+        self._pixels[x+1] = self._pixels[x+1] & m
+        if color&1:
+            self._pixels[x] = self._pixels[x] | y
+        if color&2:
+            self._pixels[x+1] = self._pixels[x+1] | y
+            
+    def draw_sprite(self,x,y,sprite):
+        for yo in range(len(sprite)):
+            for xo in range(len(sprite[yo])):
+                color = sprite[yo][xo]
+                if color=='.':
+                    color = 0
+                else:
+                    color = int(color)
+                self.set_pixel(x+xo,y+yo,color)
 
 class LightButton:
     
--- pi/test.py
+++ pi/test.py
@@ -1,15 +1,23 @@
-import smbus
-bus = smbus.SMBus(1)
 
 ADDRESS = 0x72
 
 from hardware import LED8x8
-from hardware import LightButton
+#from hardware import LightButton
 
-disp = LED8x8(bus,ADDRESS)
+#but = LightButton(11,13,15)
+#print(str(but.get_button()))
+#but.set_color(0)
 
-but = LightButton(11,13,15)
+disp = LED8x8(ADDRESS)
 
-print(str(but.get_button()))
+#disp.draw_raw_image([1,0,0,0,255,0,0,0])
 
-but.set_color(0)
+#disp.draw_raw_image([0x0E,0x0E,0x05,0x05,0x2E,0x0E,0x20,0x00,0xE0,0x00,0x20,0x09,0x20,0x09,0x00,0x06])
+
+sprite = ['1111',
+          '1321',
+          '1231',
+          '1111']
+
+disp.draw_sprite(0,2,sprite)
+disp.refresh_display()
