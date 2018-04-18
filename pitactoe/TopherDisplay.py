ab914ac976653b5a70485367ff6c87668eec767a
Chris Cantrell <chris.cantrell@vertivco.com>
2018-04-18 13:48:25 -0500

    Working on an imperative implementation on the pi in Python ... for contrast

---
+++ pitactoe/TopherDisplay.py
@@ -0,0 +1,59 @@
+from Adafruit_LED_Backpack import BicolorMatrix8x8
+
+# Copied from the baseclass.
+
+# Color values as convenient globals.
+# This is a bitmask value where the first bit is green, and the second bit is
+# red.  If both bits are set the color is yellow (red + green light).
+OFF = 0
+GREEN = 1
+RED = 2
+YELLOW = 3
+
+class TopherDisplay(BicolorMatrix8x8.BicolorMatrix8x8):
+    
+    def __init__(self, **kwargs):
+        """Initialize display.  All arguments will be passed to the HT16K33 class
+        initializer, including optional I2C address and bus number parameters.
+        """
+        super(TopherDisplay, self).__init__(**kwargs)
+        
+    def set_pixel(self, x, y, value):
+        """Set pixel at position x, y to the given value.  X and Y should be values
+        of 0 to 8.  Value should be OFF, GREEN, RED, or YELLOW.
+        """
+        
+        # Rotation and mirroring
+        a = x
+        x = y
+        y = 7-a
+        
+        # From the baseclass
+        if x < 0 or x > 7 or y < 0 or y > 7:
+            # Ignore out of bounds pixels.
+            return
+        # Set green LED based on 1st bit in value.
+        self.set_led(y * 16 + x, 1 if value & GREEN > 0 else 0)
+        # Set red LED based on 2nd bit in value.
+        self.set_led(y * 16 + x + 8, 1 if value & RED > 0 else 0)
+        
+    def draw_ascii_image(self, img, xo, yo):
+        for y in range(len(img)):
+            for x in range(len(img[y])):
+                c = img[y][x]
+                v = OFF
+                if c=='R':
+                    v = RED
+                elif c=='G':
+                    v = GREEN
+                elif c=='Y':
+                    v = YELLOW
+                self.set_pixel(x+xo,y+yo,v)
+                
+    def draw_v_line(self,x,color):
+        for y in range(8):
+            self.set_pixel(x,y,color)
+            
+    def draw_h_line(self,y,color):
+        for x in range(8):
+            self.set_pixel(x,y,color)
---
+++ pitactoe/tictactoe.py
@@ -0,0 +1,83 @@
+from TopherDisplay import TopherDisplay
+
+display = TopherDisplay(address=0x72)
+display.begin()
+display.clear()
+
+i_ticT = ["GGG",
+          ".G.",
+          ".G.",
+          ".G."]
+i_ticI = ["RRR",
+          ".R.",
+          ".R.",
+          "RRR"]
+i_ticC = ["YYY",
+          "Y..",
+          "YYY"]
+
+i_tacT = ["YYY",
+          ".Y.",
+          ".Y.",
+          ".Y."]
+i_tacA = [".GG.",
+          "G..G",
+          "GGGG",
+          "G..G"]
+i_tacC = ["RRR",
+          "R..",
+          "RRR"]
+
+i_toeT = ["RRR",
+          ".R.",
+          ".R."]
+i_toeO = ["YYY",
+          "Y.Y",
+          "YYY"]
+i_toeE = ["GGGG",
+          "G...",
+          "GGG.",
+          "G...",
+          "GGGG"]
+
+"""
+def draw_h_line(display,x,c):
+    for y in range(8):
+        display.set_pixel(x,y,c)
+        
+def draw_v_line(display,y,c):
+    for x in range(8):
+        display.set_pixel(x,7-y,c)
+
+def draw_ascii_image(display, img, xo, yo):
+    for y in range(len(img)):
+        for x in range(len(img[y])):
+            c = img[y][x]
+            v = 0
+            if c=='R':
+                v = 2
+            elif c=='G':
+                v = 1
+            elif c=='Y':
+                v = 3
+            display.set_pixel(y+yo,7-(x+xo),v)
+
+draw_ascii_image(display,i_ticT,0,0)
+draw_ascii_image(display,i_ticI,4,0)
+draw_ascii_image(display,i_ticC,3,5)
+     
+draw_ascii_image(display,i_tacT,0,0)
+draw_ascii_image(display,i_tacA,4,0)
+draw_ascii_image(display,i_tacC,3,5)
+   
+draw_ascii_image(display,i_toeT,1,0)
+draw_ascii_image(display,i_toeO,5,0)
+draw_ascii_image(display,i_toeE,3,3)
+"""
+
+display.draw_h_line(4,2)
+display.draw_v_line(6,1)
+
+#display.draw_ascii_image(i_ticT,2,4)
+
+display.write_display()
